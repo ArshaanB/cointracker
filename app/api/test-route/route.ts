5fde9cb0df5ea7c0d84b742d7fc10b6e1269f308
Arshaan <arshaan.bhimani@uwaterloo.ca>
2024-10-11 00:12:34 -0400

    Adapt API for AWS Lambda, update dependencies

--- app/api/test-route/route.ts
+++ app/api/test-route/route.ts
@@ -1,10 +1,14 @@
-export async function POST(request: Request) {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
   const currentDate = new Date().toLocaleString('en-US', {
     timeZone: 'America/New_York'
   });
   console.log(`Current Date: ${currentDate}`);
-  return new Response(JSON.stringify({ message: 'success' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message: 'success' })
+  };
+};
